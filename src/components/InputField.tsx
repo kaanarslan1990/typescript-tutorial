5b716286284fcc19c290425a6414515f63671ef2
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-02-08 17:31:19 +0300

    add ınputfield

---
+++ src/components/InputField.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+const InputField = () => {
+  return (
+      <form className="input">
+          <input type="input" placeholder='Enter a task' className="input__box" />
+          <button className="input__submit" type="submit">GO</button>
+      </form>
+  )};
+
+export default InputField;
