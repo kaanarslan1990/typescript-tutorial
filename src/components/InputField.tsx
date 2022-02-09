eb81257339618f1bf2643ba609393d9f3a88eead
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-02-09 14:34:47 +0300

    add todolist and icons

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import './styles.css';
+import React, { useRef } from "react";
+import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-      <form className="input">
-          <input type="input" placeholder='Enter a task' className="input__box" />
-          <button className="input__submit" type="submit">GO</button>
-      </form>
-  )};
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input__box"
+      />
+      <button className="input__submit" type="submit">
+        GO
+      </button>
+    </form>
+  );
+};
 
 export default InputField;
